24468495622542af8f7dde48ad2bd61aa2461985
EmanuelGabrielSimiras <simirasemanuel@gmail.com>
2026-03-17 18:46:07 +0200

    Update app.py

--- Challenge Writeup-Clanker Casino/app.py
+++ Challenge Writeup-Clanker Casino/app.py
@@ -2,10 +2,10 @@
 @login_required
 def game():
     if request.method == 'POST':
-        # aici se preia raspunsul tau
+        # aici se preia raspunsul meu
         user_answer = request.form.get('captcha_answer', type=int)
         captcha_id = session.get('captcha_id')
-        # aici se cauta solutia in memorie
+        # cauta solutia in memorie
         correct_answer = captcha_storage.get(captcha_id)
         
         # dupa prima incercare, solutia este stearsa
@@ -13,7 +13,7 @@ def game():
             del captcha_storage[captcha_id]
         session.pop('captcha_id', None)
 
-        # bug-ul: daca ambele sunt None, verificarea trece
+        # bug-ul- daca ambele sunt none, verificarea trece
         if user_answer != correct_answer:
             flash("ERROR: Optical Sensors Misaligned.", "error")
             return redirect(url_for('game'))
